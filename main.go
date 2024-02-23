e8fbe4ccbcded531bf1f1c570aa430f6135e55d4
Mopcho <47301161+Mopcho@users.noreply.github.com>
2024-02-24 00:19:16 +0200

    testing components

--- main.go
+++ main.go
@@ -1,14 +1,40 @@
 package main
 
 import (
+	"fmt"
+
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 )
 
+func AppPage() (*widget.Label, *widget.Entry, *widget.Button) {
+	label := widget.NewLabel("Username:")
+	input := widget.NewEntry()
+	submitButton := widget.NewButton("Login", func() {
+		fmt.Printf("%v", input.Text)
+	})
+
+	return label, input, submitButton
+}
+
 func main() {
 	a := app.New()
-	w := a.NewWindow("Hello World")
+	w := a.NewWindow("Clock")
 
-	w.SetContent(widget.NewLabel("Hello World!"))
+	w.Resize(fyne.NewSize(100, 100))
+
+	w.SetContent(container.NewVBox(AppPage()))
+
+	// clock := widget.NewLabel("")
+	// updateTime(clock)
+
+	// w.SetContent(clock)
+	// go func() {
+	// 	for range time.Tick(time.Second) {
+	// 		updateTime(clock)
+	// 	}
+	// }()
 	w.ShowAndRun()
 }
